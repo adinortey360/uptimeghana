4e5991e1ef31ee12bac3a2d1dbe89a1837fcaf52
Adinortey Lawerteh <adinortey360@gmail.com>
2023-04-11 01:00:57 +0000

    dummy data

--- server.py
+++ server.py
@@ -5,13 +5,32 @@ import datetime
 
 app = Flask(__name__)
 
+# TODO: Use database instead of list
+sites = [
+    ('mtngh', 'MTN', 'https://mtn.com.gh', 'telecom', 200, False),
+    ('vodafonegh', 'Vodafone', 'https://vodafone.com.gh', 'telecom', 5000, False),
+    ('airtelgh', 'Airtel', 'https://airtel.com.gh', 'telecom', 855, True),
+    ('glogh', 'Glo', 'https://glo.com.gh', 'telecom', 234, False),
+    ('airteltigo', 'AirtelTigo', 'https://airteltigo.com', 321, 'telecom', False),
+    ('ghanagov', 'Ghana.GOV', 'https://ghana.gov.gh', 'gov', 345, False),
+    ('ghanaweb', 'GhanaWeb', 'https://ghanaweb.com', 'news', 242, True),
+    ('citifmonline', 'CitiFM', 'https://citifmonline.com', 'news', 2, False),
+    ('graphic', 'Graphic', 'https://graphic.com.gh', 'news', 263, False),
+    ('gra', 'GRA', 'https://gra.gov.gh', 'gov',  58, False),
+    ('ghanahealthservice', 'Ghana Health Service', 'https://ghanahealthservice.org', 'gov', 3, False),
+]
+
+
+
 @app.route('/')
 def index():
-    conn = sqlite3.connect('sites.db')
-    cursor = conn.cursor()
-    cursor.execute('SELECT name, url, downtime FROM sites')
-    sites = cursor.fetchall()
-    conn.close()
+    # conn = sqlite3.connect('sites.db')
+    # cursor = conn.cursor()
+    # cursor.execute('SELECT name, url, downtime FROM sites')
+    # sites = cursor.fetchall()
+    # conn.close()
+
+    
 
     status_dict = {}
     for site in sites:
