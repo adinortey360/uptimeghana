ae1d3db011fa8e10528b896fd3d202a8c3e0fc4a
dev2me <eugene2essel@gmail.com>
2023-04-12 12:40:18 -0400

    added create and update func

--- database/db_logic.py
+++ database/db_logic.py
@@ -9,4 +9,28 @@ def create_connection(db_file):
     except sqlite3.Error as e:
         print(e)
     return conn
+
+def create_entry(conn, entry):
+    sql = '''INSERT INTO sites(name, company, url, category, popularity, image, downtime)
+             VALUES(?, ?, ?, ?, ?, ?, ?)'''
+    cursor = conn.cursor()
+    cursor.execute(sql, entry)
+    conn.commit()
+    conn.close()
     
+
+
+def update_entry(conn, entry):
+    sql = '''UPDATE sites 
+             SET name=?, 
+                 company=?, 
+                 url=?, 
+                 category=?,
+                 popularity=?,
+                 image=?,
+                 downtime=?,
+             WHERE id=?'''
+    cursor = conn.cursor()
+    cursor.execute(sql, entry['id'])
+    conn.commit()
+    conn.close()
